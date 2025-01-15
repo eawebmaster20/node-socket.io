cebe233d5dda665aea25d5f079822eb1d93b835d
eawebmaster20 <eaweb20@gmail.com>
2025-01-15 23:34:38 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+var admin = require("firebase-admin");
+var bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(cors());
+
+const server = http.createServer(app);
+
+// Configure Socket.IO with CORS
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:4201', // Replace with your Angular app's URL
+    methods: ['GET', 'POST'],       // Allowed HTTP methods
+    allowedHeaders: ['Content-Type'], // Allowed headers
+    credentials: true               // Allow cookies and credentials
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  // Listen to events
+  socket.on('message', (msg) => {
+    console.log('Message received:', msg);
+    // Emit an event to all clients
+    io.emit('message', `Server received: ${msg}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Socket.IO server is running on port ${PORT}`);
+});
