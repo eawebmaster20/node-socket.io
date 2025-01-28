24dde2bb6ac36dbe08bc6cdb76b9a4e035698e66
Emmanuel Adomako <102923937+eawebmaster20@users.noreply.github.com>
2025-01-28 10:13:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -16,41 +16,44 @@ app.use(cors({
   credentials: true, // Allow credentials
 }));
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-// Configure Socket.IO with CORS
-const io = new Server(server, {
-  cors: {
-    origin: true, // Replace with your Angular app's URL
-    methods: ['GET', 'POST'],       // Allowed HTTP methods
-    allowedHeaders: ['Content-Type'], // Allowed headers
-    credentials: true               // Allow cookies and credentials
-  }
-});
+// // Configure Socket.IO with CORS
+// const io = new Server(server, {
+//   cors: {
+//     origin: true, // Replace with your Angular app's URL
+//     methods: ['GET', 'POST'],       // Allowed HTTP methods
+//     allowedHeaders: ['Content-Type'], // Allowed headers
+//     credentials: true               // Allow cookies and credentials
+//   }
+// });
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
+// io.on('connection', (socket) => {
+//   console.log('A user connected:', socket.id);
 
-  // Listen to events
-  socket.on('message', (msg) => {
-    console.log('Message received:', msg);
-    const payload = {
-      title: msg.title || 'notification app',
-      description: msg.body || 'transformer voltage is 300v',
-      img: '',
-      time: new Date().toISOString(),
-      status: 'warning', 
-    }
-    // Emit an event to all clients
-    io.emit('message', payload);
-  });
+//   // Listen to events
+//   socket.on('message', (msg) => {
+//     console.log('Message received:', msg);
+//     const payload = {
+//       title: msg.title || 'notification app',
+//       description: msg.body || 'transformer voltage is 300v',
+//       img: '',
+//       time: new Date().toISOString(),
+//       status: 'warning', 
+//     }
+//     // Emit an event to all clients
+//     io.emit('message', payload);
+//   });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
-});
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected:', socket.id);
+//   });
+// });
 
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Socket.IO server is running on port ${PORT}`);
-});
+// // Start the server
+// server.listen(PORT, () => {
+//   console.log(`Socket.IO server is running on port ${PORT}`);
+// });
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+})
