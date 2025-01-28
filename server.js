af206da25a8d108aed5bab75a3b4d6d73c86de5a
eawebmaster20 <eaweb20@gmail.com>
2025-01-28 12:20:49 +0000

    chore: revert back to socket io server

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const cors = require('cors');
 const { Server } = require('socket.io');
 var admin = require("firebase-admin");
 var bodyParser = require('body-parser');
-const { Sequelize } = require('sequelize');
-const sequelizeWithUrl = new Sequelize('postgres://user:pass@example.com:5432/dbname')
-const sequelize = new Sequelize('railway', 'postgres', 'YTGBkitMWNMvQFlNaZVgESznGdgmTZzB', {
-  host: 'postgres-gtqw.railway.internal',
-  dialect: 'postgres'
-});
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.urlencoded());
@@ -21,47 +16,41 @@ app.use(cors({
   credentials: true, // Allow credentials
 }));
 
-app.get('/', (req, res)=>{
-  res.json('server is working so, Hola!');
-})
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-// // Configure Socket.IO with CORS
-// const io = new Server(server, {
-//   cors: {
-//     origin: true, // Replace with your Angular app's URL
-//     methods: ['GET', 'POST'],       // Allowed HTTP methods
-//     allowedHeaders: ['Content-Type'], // Allowed headers
-//     credentials: true               // Allow cookies and credentials
-//   }
-// });
+// Configure Socket.IO with CORS
+const io = new Server(server, {
+  cors: {
+    origin: true, // Replace with your Angular app's URL
+    methods: ['GET', 'POST'],       // Allowed HTTP methods
+    allowedHeaders: ['Content-Type'], // Allowed headers
+    credentials: true               // Allow cookies and credentials
+  }
+});
 
-// io.on('connection', (socket) => {
-//   console.log('A user connected:', socket.id);
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
 
-//   // Listen to events
-//   socket.on('message', (msg) => {
-//     console.log('Message received:', msg);
-//     const payload = {
-//       title: msg.title || 'notification app',
-//       description: msg.body || 'transformer voltage is 300v',
-//       img: '',
-//       time: new Date().toISOString(),
-//       status: 'warning', 
-//     }
-//     // Emit an event to all clients
-//     io.emit('message', payload);
-//   });
+  // Listen to events
+  socket.on('message', (msg) => {
+    console.log('Message received:', msg);
+    const payload = {
+      title: msg.title || 'notification app',
+      description: msg.body || 'transformer voltage is 300v',
+      img: '',
+      time: new Date().toISOString(),
+      status: 'warning', 
+    }
+    // Emit an event to all clients
+    io.emit('message', payload);
+  });
 
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected:', socket.id);
-//   });
-// });
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
 
-// // Start the server
-// server.listen(PORT, () => {
-//   console.log(`Socket.IO server is running on port ${PORT}`);
-// });
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-})
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Socket.IO server is running on port ${PORT}`);
+});
